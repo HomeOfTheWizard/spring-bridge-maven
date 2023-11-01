de9d2abef990733b07e35e3422af178113c1d6fe
Dave Syer <dsyer@vmware.com>
2023-11-01 11:34:26 +0000

    Use Spring resource abstraction to load properties

--- library/src/main/java/com/homeofthewizard/PropertyBasedConfigurableSpringModule.java
+++ library/src/main/java/com/homeofthewizard/PropertyBasedConfigurableSpringModule.java
@@ -1,16 +1,6 @@
 package com.homeofthewizard;
 
-import org.apache.maven.model.Dependency;
-import org.apache.maven.model.Model;
-import org.apache.maven.model.io.xpp3.MavenXpp3Writer;
-import org.apache.maven.shared.invoker.*;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.core.env.PropertySource;
-import org.springframework.guice.module.SpringModule;
-
-import javax.inject.Named;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -18,48 +8,73 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
+import javax.inject.Named;
+
+import org.apache.maven.model.Dependency;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Writer;
+import org.apache.maven.shared.invoker.DefaultInvocationRequest;
+import org.apache.maven.shared.invoker.DefaultInvoker;
+import org.apache.maven.shared.invoker.InvocationRequest;
+import org.apache.maven.shared.invoker.Invoker;
+import org.apache.maven.shared.invoker.MavenInvocationException;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+import org.springframework.guice.module.SpringModule;
+import org.springframework.util.StringUtils;
 
 @Named
 class PropertyBasedConfigurableSpringModule extends SpringModule {
 
     private final static String SPRING_EXT = "spring-ext";
-    private final static String CONFIG_FILE_PATH = SPRING_EXT + "." +"configFilePath";
+    private final static String CONFIG_FILE_PATH = SPRING_EXT + "." + "configFilePath";
     private final static String CONTEXT_CONFIG_CLASS = SPRING_EXT + "." + "contextConfigClass";
     private final static String SPRING_MAVEN_EXTENSION_PROPERTIES = "SpringMavenExtensionProperties";
-    private final static String DEFAULT_PROPERTIES_FILE_PATH = "/src/main/resources/" + SPRING_EXT + ".properties";
-    private final static  String CLASSPATH_FILE = System.getProperty("java.io.tmpdir") + File.separator + "cp.txt";
+    private final static String DEFAULT_PROPERTIES_FILE_PATH = SPRING_EXT + ".properties";
+    private final static String CLASSPATH_FILE = System.getProperty("java.io.tmpdir") + File.separator + "cp.txt";
     private final static Properties properties = new Properties();
 
-    static{
+    static {
         loadProperties();
     }
 
-    static void loadProperties(){
+    static void loadProperties() {
         try {
-            var filePath = Objects.requireNonNullElse(
-                    System.getProperty(CONFIG_FILE_PATH),
-                    new File(".").getCanonicalPath() + DEFAULT_PROPERTIES_FILE_PATH
-            );
-            var inStream = new FileInputStream(filePath);
-            properties.load(inStream);
+            var path = StringUtils.cleanPath(System.getProperty(CONFIG_FILE_PATH, DEFAULT_PROPERTIES_FILE_PATH));
+            var resource = path.equals(DEFAULT_PROPERTIES_FILE_PATH)
+                    ? new ClassPathResource(DEFAULT_PROPERTIES_FILE_PATH,
+                            PropertyBasedConfigurableSpringModule.class.getClassLoader())
+                    : path.contains(":") ? new UrlResource(System.getProperty(CONFIG_FILE_PATH))
+                            : new FileSystemResource(path);
+            PropertiesLoaderUtils.fillProperties(properties, resource);
         } catch (Exception e) {
+            e.printStackTrace();
             throw new RuntimeException(e);
         }
     }
 
-    public PropertyBasedConfigurableSpringModule() throws ClassNotFoundException, IOException, MavenInvocationException {
+    public PropertyBasedConfigurableSpringModule()
+            throws ClassNotFoundException, IOException, MavenInvocationException {
         super(new SpringApplicationBuilder()
                 .sources(loadSpringConfigClass())
                 .initializers(context -> context
                         .getEnvironment()
                         .getPropertySources()
-                        .addLast(new SpringExtensionPropertySource(SPRING_MAVEN_EXTENSION_PROPERTIES))
-                )
+                        .addLast(new SpringExtensionPropertySource(SPRING_MAVEN_EXTENSION_PROPERTIES)))
                 .run());
     }
 
-    static private Class<?> loadSpringConfigClass() throws ClassNotFoundException, IOException, MavenInvocationException {
+    static private Class<?> loadSpringConfigClass()
+            throws ClassNotFoundException, IOException, MavenInvocationException {
 
         var pomFile = createDependenciesPom();
         var filePath = createClassPathFile(pomFile);
@@ -90,17 +105,17 @@ class PropertyBasedConfigurableSpringModule extends SpringModule {
 
     private static String createClassPathFile(File pomFile) throws MavenInvocationException {
         InvocationRequest request = new DefaultInvocationRequest();
-        request.setPomFile( pomFile );
-        request.setGoals( Arrays.asList( "dependency:resolve", "dependency:build-classpath") );
+        request.setPomFile(pomFile);
+        request.setGoals(Arrays.asList("dependency:resolve", "dependency:build-classpath"));
         request.setMavenOpts("-Dmdep.outputFile=" + CLASSPATH_FILE);
         Invoker invoker = new DefaultInvoker();
-        invoker.execute( request );
+        invoker.execute(request);
         return CLASSPATH_FILE;
     }
 
     private static String[] readClassPathFile(String filePath) throws IOException {
         var cpFile = Path.of(filePath);
-        try(var bufferedReader = Files.newBufferedReader(cpFile)) {
+        try (var bufferedReader = Files.newBufferedReader(cpFile)) {
             var line = bufferedReader.readLine();
             return line.split(File.pathSeparator);
         }
@@ -109,7 +124,7 @@ class PropertyBasedConfigurableSpringModule extends SpringModule {
     private static ClassLoader updateClassLoaderWithDependenciesClassPath(String[] paths) {
         List<URL> urls = new ArrayList<>();
         try {
-            for(var path : paths){
+            for (var path : paths) {
                 urls.add(new File(path).toURI().toURL());
             }
         } catch (MalformedURLException e) {
@@ -118,13 +133,12 @@ class PropertyBasedConfigurableSpringModule extends SpringModule {
 
         ClassLoader contextClassLoader = URLClassLoader.newInstance(
                 urls.toArray(new URL[0]),
-                PropertyBasedConfigurableSpringModule.class.getClassLoader()
-        );
+                PropertyBasedConfigurableSpringModule.class.getClassLoader());
 
         return contextClassLoader;
     }
 
-    private static class SpringExtensionPropertySource extends PropertySource<String>{
+    private static class SpringExtensionPropertySource extends PropertySource<String> {
         public SpringExtensionPropertySource(String name) {
             super(name);
         }
