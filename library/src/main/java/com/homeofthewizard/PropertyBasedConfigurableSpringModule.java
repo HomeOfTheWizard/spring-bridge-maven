263bf20272d9803ec1965787d7e839cebecacd9f
Dave Syer <dsyer@vmware.com>
2023-11-01 11:47:09 +0000

    Use ambient class loader instead of cp.txt

--- library/src/main/java/com/homeofthewizard/PropertyBasedConfigurableSpringModule.java
+++ library/src/main/java/com/homeofthewizard/PropertyBasedConfigurableSpringModule.java
@@ -1,27 +1,10 @@
 package com.homeofthewizard;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Properties;
 
 import javax.inject.Named;
 
-import org.apache.maven.model.Dependency;
-import org.apache.maven.model.Model;
-import org.apache.maven.model.io.xpp3.MavenXpp3Writer;
-import org.apache.maven.shared.invoker.DefaultInvocationRequest;
-import org.apache.maven.shared.invoker.DefaultInvoker;
-import org.apache.maven.shared.invoker.InvocationRequest;
-import org.apache.maven.shared.invoker.Invoker;
 import org.apache.maven.shared.invoker.MavenInvocationException;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.core.env.PropertySource;
@@ -40,7 +23,6 @@ class PropertyBasedConfigurableSpringModule extends SpringModule {
     private final static String CONTEXT_CONFIG_CLASS = SPRING_EXT + "." + "contextConfigClass";
     private final static String SPRING_MAVEN_EXTENSION_PROPERTIES = "SpringMavenExtensionProperties";
     private final static String DEFAULT_PROPERTIES_FILE_PATH = SPRING_EXT + ".properties";
-    private final static String CLASSPATH_FILE = System.getProperty("java.io.tmpdir") + File.separator + "cp.txt";
     private final static Properties properties = new Properties();
 
     static {
@@ -75,67 +57,8 @@ class PropertyBasedConfigurableSpringModule extends SpringModule {
 
     static private Class<?> loadSpringConfigClass()
             throws ClassNotFoundException, IOException, MavenInvocationException {
-
-        var pomFile = createDependenciesPom();
-        var filePath = createClassPathFile(pomFile);
-        var paths = readClassPathFile(filePath);
-        var classLoader = updateClassLoaderWithDependenciesClassPath(paths);
-
+        var classLoader = PropertyBasedConfigurableSpringModule.class.getClassLoader();
         return classLoader.loadClass(properties.getProperty(CONTEXT_CONFIG_CLASS));
-    }
-
-    private static File createDependenciesPom() throws IOException {
-        var modelCustom = new Model();
-        modelCustom.setModelVersion("4.0.0");
-        modelCustom.setGroupId("com.homeofthewizard");
-        modelCustom.setArtifactId("spring-extension-dependencies");
-        modelCustom.setVersion("1.0.0");
-
-        var dependency = new Dependency();
-        dependency.setGroupId(properties.getProperty(SPRING_EXT + ".groupId"));
-        dependency.setArtifactId(properties.getProperty(SPRING_EXT + ".artifactId"));
-        dependency.setVersion(properties.getProperty(SPRING_EXT + ".version"));
-        modelCustom.addDependency(dependency);
-
-        var writer = new MavenXpp3Writer();
-        File file = File.createTempFile("pom", ".xml");
-        writer.write(new FileOutputStream(file), modelCustom);
-        return file;
-    }
-
-    private static String createClassPathFile(File pomFile) throws MavenInvocationException {
-        InvocationRequest request = new DefaultInvocationRequest();
-        request.setPomFile(pomFile);
-        request.setGoals(Arrays.asList("dependency:resolve", "dependency:build-classpath"));
-        request.setMavenOpts("-Dmdep.outputFile=" + CLASSPATH_FILE);
-        Invoker invoker = new DefaultInvoker();
-        invoker.execute(request);
-        return CLASSPATH_FILE;
-    }
-
-    private static String[] readClassPathFile(String filePath) throws IOException {
-        var cpFile = Path.of(filePath);
-        try (var bufferedReader = Files.newBufferedReader(cpFile)) {
-            var line = bufferedReader.readLine();
-            return line.split(File.pathSeparator);
-        }
-    }
-
-    private static ClassLoader updateClassLoaderWithDependenciesClassPath(String[] paths) {
-        List<URL> urls = new ArrayList<>();
-        try {
-            for (var path : paths) {
-                urls.add(new File(path).toURI().toURL());
-            }
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
-
-        ClassLoader contextClassLoader = URLClassLoader.newInstance(
-                urls.toArray(new URL[0]),
-                PropertyBasedConfigurableSpringModule.class.getClassLoader());
-
-        return contextClassLoader;
     }
 
     private static class SpringExtensionPropertySource extends PropertySource<String> {
