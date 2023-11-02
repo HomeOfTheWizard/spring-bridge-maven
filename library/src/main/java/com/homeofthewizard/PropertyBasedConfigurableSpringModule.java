712e21ef52124ab8cdece33f9920e03f17ace495
Dave Syer <dsyer@vmware.com>
2023-11-02 06:26:04 +0000

    Use spring.factories and application.properties

--- library/src/main/java/com/homeofthewizard/PropertyBasedConfigurableSpringModule.java
+++ library/src/main/java/com/homeofthewizard/PropertyBasedConfigurableSpringModule.java
@@ -1,73 +1,34 @@
 package com.homeofthewizard;
 
 import java.io.IOException;
-import java.util.Properties;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Named;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.core.env.PropertySource;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.UrlResource;
-import org.springframework.core.io.support.PropertiesLoaderUtils;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.support.SpringFactoriesLoader;
 import org.springframework.guice.module.SpringModule;
-import org.springframework.util.StringUtils;
 
 @Named
 class PropertyBasedConfigurableSpringModule extends SpringModule {
 
-    private final static String SPRING_EXT = "spring-ext";
-    private final static String CONFIG_FILE_PATH = SPRING_EXT + "." + "configFilePath";
-    private final static String CONTEXT_CONFIG_CLASS = SPRING_EXT + "." + "contextConfigClass";
-    private final static String SPRING_MAVEN_EXTENSION_PROPERTIES = "SpringMavenExtensionProperties";
-    private final static String DEFAULT_PROPERTIES_FILE_PATH = SPRING_EXT + ".properties";
-    private final static Properties properties = new Properties();
-
-    static {
-        loadProperties();
-    }
-
-    static void loadProperties() {
-        try {
-            var path = StringUtils.cleanPath(System.getProperty(CONFIG_FILE_PATH, DEFAULT_PROPERTIES_FILE_PATH));
-            var resource = path.equals(DEFAULT_PROPERTIES_FILE_PATH)
-                    ? new ClassPathResource(DEFAULT_PROPERTIES_FILE_PATH,
-                            PropertyBasedConfigurableSpringModule.class.getClassLoader())
-                    : path.contains(":") ? new UrlResource(System.getProperty(CONFIG_FILE_PATH))
-                            : new FileSystemResource(path);
-            PropertiesLoaderUtils.fillProperties(properties, resource);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
-    }
-
     public PropertyBasedConfigurableSpringModule()
             throws ClassNotFoundException, IOException {
-        super(new SpringApplicationBuilder()
-                .sources(loadSpringConfigClass())
-                .initializers(context -> context
-                        .getEnvironment()
-                        .getPropertySources()
-                        .addLast(new SpringExtensionPropertySource(SPRING_MAVEN_EXTENSION_PROPERTIES)))
-                .run());
+        super(new SpringApplicationBuilder(new DefaultResourceLoader(PropertyBasedConfigurableSpringModule.class.getClassLoader()))
+                .sources(loadSpringConfigClass()).run());
     }
 
-    static private Class<?> loadSpringConfigClass()
+    static private Class<?>[] loadSpringConfigClass()
             throws ClassNotFoundException, IOException {
         var classLoader = PropertyBasedConfigurableSpringModule.class.getClassLoader();
-        return classLoader.loadClass(properties.getProperty(CONTEXT_CONFIG_CLASS));
+        List<String> names = SpringFactoriesLoader.loadFactoryNames(SpringBootPlugin.class, classLoader);
+        List<Class<?>> types = new ArrayList<>();
+        for (String name : names) {
+            types.add(classLoader.loadClass(name));
+        }
+        return types.toArray(new Class<?>[0]);
     }
 
-    private static class SpringExtensionPropertySource extends PropertySource<String> {
-        public SpringExtensionPropertySource(String name) {
-            super(name);
-        }
-
-        @Override
-        public Object getProperty(String name) {
-            return properties.getProperty(name);
-        }
-    }
 }
--- src/it/example/plugin/src/main/java/com/homeofthewizard/plugin/MyMojo.java
+++ src/it/example/plugin/src/main/java/com/homeofthewizard/plugin/MyMojo.java
@@ -3,11 +3,14 @@ package com.homeofthewizard.plugin;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import com.homeofthewizard.SpringBootPlugin;
 import com.homeofthewizard.friends.MyHelloer;
+import com.homeofthewizard.friends.MySpringConfiguration;
 
 import javax.inject.Inject;
 
 @Mojo(name = "run", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
+@SpringBootPlugin(MySpringConfiguration.class)
 public class MyMojo extends AbstractMojo {
 
     private final MyHelloer myFriend;
