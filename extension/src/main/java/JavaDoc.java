686baca098f49ea2562b74465e723a4eced524eb
Ozgun OZ <ozgunn.oz@gmail.com>
2024-07-14 14:07:44 +0200

    fix maven central publishing conditions

---
+++ extension/src/main/java/JavaDoc.java
@@ -0,0 +1,7 @@
+/**
+ * This is a class only to workaround the following issue and generate a javadoc for the project.
+ * https://bugs.openjdk.org/browse/JDK-4492654
+ * The project does not contain any java class. The jar package of this project contains only a maven extension config file: META-INF/maven/extension.xml
+ */
+public class JavaDoc {
+}
